fc13b2cff9060f7f4ee07b5da848cc031116b3ab
raymed2005 <111724429+raymed2005@users.noreply.github.com>
2023-12-25 20:39:42 -0800

    Add files via upload

---
+++ ReferencePointerSwap.cpp
@@ -0,0 +1,36 @@
+// *******************************************************************************
+// Assignment 10C: Reference Pointer Swap
+// Author: Rayyan Ahmed
+// Description: Swaps pointers and nums
+// Status: Complete
+// Date: December 1, 2023
+// ********************************************************************************
+#include <iostream>
+using namespace std;
+
+void referenceSwap(int&, int&);
+void pointerSwap(int*, int*);
+
+int main() {
+    int num1 = 1, num2 = 2;
+    int* ptr1 = &num1;
+    int* ptr2 = &num2;
+    cout << "Before swapping nums: " << num1 << " and " << num2 << endl;
+    referenceSwap(num1, num2);
+    cout << "After swapping nums: " << num1 << " and " << num2 << endl;
+    cout << "Before swapping pointers: " << *ptr1 << " and " << *ptr2 << endl;
+    pointerSwap(ptr1, ptr2);
+    cout << "After swapping pointers: " << *ptr1 << " and " << *ptr2 << endl;
+}
+
+void referenceSwap(int& num1, int& num2){
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+void pointerSwap(int* ptr1, int* ptr2){
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
