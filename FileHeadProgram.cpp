ad1c0c85bd511b27755547438a4584fdf7488527
raymed2005 <111724429+raymed2005@users.noreply.github.com>
2023-12-25 20:34:23 -0800

    Add files via upload

---
+++ FileHeadProgram.cpp
@@ -0,0 +1,44 @@
+// *******************************************************************************
+// Assignment 7A: File Head Program
+// Author: Rayyan Ahmed
+// Description: Displays first ten lines of users file
+// Status: Complete
+// Date: October 29, 2023
+// ********************************************************************************
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+void openFile(ifstream&, string);
+
+int main(){
+    string fileName;
+    string currLine;
+    int lineCount = 1;
+
+    cout << "Enter file name: ";
+    cin >> fileName;
+    ifstream userFile;
+    openFile(userFile, fileName);
+
+    while(lineCount <= 10){
+        getline(userFile, currLine);
+        cout << "Line " << lineCount << ": " << currLine << endl;
+        lineCount++;
+    }
+    if(userFile.eof()){
+        cout << "Entire file has been displayed.";
+    }
+    userFile.close();
+}
+
+void openFile(ifstream& userFile, string fileName){
+    userFile.open(fileName);
+    if(userFile.is_open()){
+        cout << "File has been opened successfully." << endl;
+    } else {
+        cout << "Error, could not open file.";
+        exit(-1);
+    }
+}
+
